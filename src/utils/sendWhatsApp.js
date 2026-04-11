52e39d55cdcf2641b1c4c231d7d104155878a4ad
Rajnish Kumar <119472379+RajnishGautam@users.noreply.github.com>
2026-04-11 23:17:37 +0530

    remaining balance error

--- src/utils/sendWhatsApp.js
+++ src/utils/sendWhatsApp.js
@@ -98,8 +98,7 @@ export const sendFeeReminder = async ({
     `📚 *${inst}*\n\n` +
     `Dear Parent of *${studentName}*,\n\n` +
     `🗓️ *${currentMonth} Fee Reminder*\n` +
-    // `Monthly Fee: *₹${monthlyFee}*\n` +
-    `Monthly Fee: *₹${remainingBalance}*\n` +
+    `Monthly Fee: *₹${monthlyFee}*\n` +
     `Please pay your fees at the earliest.\n\n` +
     `📊 *${lastMonthName} Attendance*\n` +
     `✅ Present: ${present} days\n` +
